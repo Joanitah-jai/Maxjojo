1bf9ee179f94751332d71a1be0c9cc349520ac40
Joanitah Nankya <joanitahnankya2021@gmail.com>
2025-04-04 00:26:22 +0300

    modified the settings file

--- calibration_management_system/settings.py
+++ calibration_management_system/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'calibration_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'calibration/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
